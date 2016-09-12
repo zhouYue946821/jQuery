d8dc1704cb801f39c1ef37f43a3e8601a4a387d0
zhouYue946821 <m13514523740@163.com>
2016-09-12 10:22:06 +0800

    更改了内容.pageX/pageY

--- 图片放大效果.jQuery/js/tuPianFangDa.js
+++ 图片放大效果.jQuery/js/tuPianFangDa.js
@@ -10,11 +10,12 @@
 	    $olb = $divB.find('ol'),
 	    $span = $divA.find('.div-span'),
 	    $olbLi = $olb.find('li'),
-	    width = $divA.width(),
-	    n = 450/width,
+	    width = $ulaLi.width(),
+	    spanW = $span.width(),
+	    spanH = $span.height(),
 	    $a = $div.find('a'),
+	    n = 450/width,
 	    height = $divA.height();
-	    console.log($a)
 	    
 	    $olbLi.on('click',function () {
 	    	var $this = $(this),
@@ -38,28 +39,32 @@
 	    	$span.add($divXian).hide();
 	    })
 	    
-/*	    $divA.hover(function () {
-	    	$span.add($divXian).show();
-	    	
-	    },function () {
-	    	$span.add($divXian).hide();
-	    })	   
-	    .on('mousemove',function (e) {
-			var x = e.clientX;
-			var y = e.clientY;
-	
-			$span.css(
-				{
-					top:y-50,
-					left:x-50
-				});
-	   });*/
-	
-	
 	    $divA.on('mousemove',function (e) {
-			var x = e.clientX;
-			var y = e.clientY;
-			console.log(x)
+	    	
+	    	var x = e.pageX - $divA.offset().left - spanW/2;
+	    	var y = e.pageY - $divA.offset().top - spanH/2;
+
+	    	
+			var by = e.pageY - $divXian.height() / 2;
+			var bx = e.pageX - $divXian.width() / 2;	    	
+	    	console.log(x)
+			if (x+spanW>width) {
+				x = width-spanW;
+			}else if (x<=0) {
+				x = 0;
+			}
+			if (y+spanH>height) {
+				y = height - spanH;
+			}else if (y<=0) {
+				y = 0;
+			};	    	
+	    	$span.css({top:y,left:x});
+	    	$ulXLi
+	    	.css('background-position',-bx + 'px'+ ' ' + (-by) + 'px' );
+	    	
+/*			var x = e.pageX;
+			var y = e.pageY;
+			console.log(e)
 			if (x+50>width) {
 				x = width-50;
 			}else if (x<50) {
@@ -69,28 +74,29 @@
 				y = height - 50;
 			}else if (y<50) {
 				y = 50;
-			}			
+			}		
 			$span.css({top:y-50,left:x-50});	
 			$ulXLi
-			.css('background-position',-x*n+100 + 'px'+ ' ' + (-y*n +100) + 'px' )
+			.css('background-position',-x*n+100 + 'px'+ ' ' + (-y*n +100) + 'px' )*/
 	   });
 	   
 	   $a.on('click',function () {
-	   	var $this = $(this);
+	   	var $this = $(this);	   	
 	   	    if ($this.hasClass('color')) {
 	   	        $olb.animate({'margin-left':'-65px'},500);
-	   	        //$this.css('backgroundColor','#ccc')
-	   	        $this
-	   	        .removeClass('color')
-	   	        .siblings('a.right')
-	   	        .addClass('color');
 	   	    }else {
 	   	    	$olb.animate({'margin-left':0},500)
-	   	    	$this
-	   	        .removeClass('color')
-	   	        .siblings('a.left')
-	   	        .addClass('color');	   	    	
-	   	    }; 	   
+	   	    }; 	
+	   	    console.log($a.eq(0))
+   	    
+	   	    if ($olb.css('margin-left')=='0px') {
+	   	    	$this.removeClass('color');
+	   	    	$a.eq(1).addClass('color');
+	   	    };
+	   	    if($olb.css('margin-left','-65px')) {
+	   	    	$a.eq(1).removeClass('color');
+	   	    	$a.eq(0).addClass('color');
+	   	    };		   	    
 	   });
 	   
 }();
